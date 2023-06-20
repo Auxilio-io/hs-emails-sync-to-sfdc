ef53593a79b05348377267918e79163590f62d00
louischausse-auxilio <lchausse@auxilio.io>
2023-06-20 15:26:07 -0400

    final version

--- get-email.js
+++ get-email.js
@@ -1,33 +1,59 @@
-//use this in a contact workflow with the trigger "Last marketing email send date is known" ans re-enrollment set to "ON"
+// to view all commits view original repo: https://github.com/Auxilio-io/hubspot-custom-coded-actions-private/blob/main/get-data-such-as-url-and-the-preview-link-of-the-last-marketing-email-sent-to-a-contact.js
 
 const axios = require('axios')
-const accessToken = process.env.OPS_TOKEN
+const accessToken = process.env.HS_EMAILS_TO_SFDC
 
 exports.main = async (event, callback) => {
-  // get last email campaign id
-  // use this endpoint: GET https://api.hubapi.com/email/public/v1/events?recipent=lchausse@auxilio.io see doc: https://legacydocs.hubspot.com/docs/methods/email/get_events?_ga=2.248456007.1708526602.1683726259-445457235.1683726259&_gl=1*14mbczk*_ga*NDQ1NDU3MjM1LjE2ODM3MjYyNTk.*_ga_LXTM6CQ0XK*MTY4MzcyNjI1OC4xLjAuMTY4MzcyNjI1OC4wLjAuMA..
 
-  // get preview link
-  axios.get('https://api.hubapi.com/marketing-emails/v1/emails/40930142514', {
-  headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
-})
-.then((res) => {
+  const emailName = event.inputFields['hs_email_last_email_name'];
 
-  const id = res.data.id 
-  const previewKey = res.data.previewKey
-  const portalId = res.data.portalId
+  //define getEmailbyName() function - see doc: https://legacydocs.hubspot.com/docs/methods/cms_email/get-all-marketing-emails
 
-  console.log(`https://${portalId}.hubspotpreview-na1.com/_hcms/preview/email/${id}?portalId=${portalId}&preview_key=${previewKey}&_preview=true&from_buffer=false&cacheBust=0`)
-})
-.catch((error) => {
-  console.error(error)
-})
+  async function getEmailbyName(name) {
 
+    const options = {
+      method: 'GET',
+      url: `https://api.hubapi.com/marketing-emails/v1/emails?name__eq=${name}&limit=300`,
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'authorization': `Bearer ${accessToken}`
+      }
+    };
 
-  callback({
-    outputFields: {
+    try {
+      const response = await axios(options);
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
     }
+
+  }
+
+  //execute function
+  const name = encodeURIComponent(emailName)
+  getEmailbyName(name)
+    .then(emailDetails => {
+    const emailbodyPlaintext = emailDetails.objects[0].emailbodyPlaintext
+    const subject = emailDetails.objects[0].subject
+    const url = emailDetails.objects[0].url 
+    const id = emailDetails.objects[0].id 
+    const previewKey = emailDetails.objects[0].previewKey
+    const portalId = emailDetails.objects[0].portalId
+    callback({
+      outputFields: {
+        subject: subject,
+        url: url,
+        emailbodyPlaintext: emailbodyPlaintext
+      }
+    });
+    console.log(subject)
+    console.log(emailbodyPlaintext)
+    console.log(url) //public url accessible by anyone - web version must be enabled in the email settings
+    console.log(`https://${portalId}.hubspotpreview-na1.com/_hcms/preview/email/${id}?portalId=${portalId}&preview_key=${previewKey}&_preview=true&from_buffer=false&cacheBust=0`) //private preview link accessible only by users loggued into the HubSpot portal
+  })
+    .catch(error => {
+    console.error(error);
   });
+
 }
